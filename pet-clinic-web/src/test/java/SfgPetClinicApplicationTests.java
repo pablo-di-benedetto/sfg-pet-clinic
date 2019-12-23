abc051912c46609cda43a3a8c02166f514d10e00
Pablo Di Benedetto <ing.pablo.dibenedetto@gmail.com>
2019-12-23 18:28:57 -0300

    Implement Interfaces for services. Closes #5

--- pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -5,10 +5,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+public class SfgPetClinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
