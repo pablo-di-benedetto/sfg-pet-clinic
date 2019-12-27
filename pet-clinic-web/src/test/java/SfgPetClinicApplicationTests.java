b2044edb04eb7fed75bbecdba8a5b6603d3ab2d1
Pablo Di Benedetto <ing.pablo.dibenedetto@gmail.com>
2019-12-27 17:10:15 -0300

    added wro4j plugin and config from Spring Pet Clinic. Closes #24

--- pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -1,4 +1,6 @@
+/*
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,3 +14,4 @@ public class SfgPetClinicApplicationTests {
     }
 
 }
+*/
